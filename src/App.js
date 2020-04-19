d863b54fcb2c635ef95a5314463658140d9e0bd1
Kapilan Navaratnam <kapilan@syomortgage.com>
2020-04-19 00:49:34 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Dog from './Dog';
 import './App.css';
 
-function App() {
+const App = () =>{
+
+
+  const [dogs, setDogs] = useState([]);
+  const [breed, setBreed] = useState('schnauzer');
+
+  /**
+   * getDogs is an asyc function
+   * response = await fetch(API)
+   * data = await response.json()
+   */
+  const getDogs = async () =>{
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+    const data = await response.json();
+    setDogs(data);
+    console.log(data);
+  }
+
+
+  useEffect( () =>{
+    getDogs();
+  }, [breed]);
+
+const handleChange = e => {
+  setBreed(e.target.value);
+}
+
+const handleSubmit = e => {
+  e.preventDefault();
+  console.log(breed);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit}>
+      <select value={breed} onChange={handleChange}> 
+        <option value="schnauzer">Schnauzer</option>
+        <option value="poodle">Poodle</option>
+        <option value="pomeranian">Pomeranian</option>
+        <option value="germanshepherd">German Shepherd</option>
+        <option value="doberman">Doberman</option>
+        <option value="pug">Pug</option>
+        <option value="terrier/russell">Russell Terrier</option>
+        <option value="maltese">Maltese</option>
+        <option value="malamute">Malamute</option>
+        <option value="husky">Husky</option>
+        <option value="dachshund">Dachshund</option>
+        <option value="boxer">Boxer</option>
+
+      </select>
+
+    </form>
+    <Dog
+    image={dogs.message} />
     </div>
-  );
-}
+
+  )
+
+} 
 
 export default App;
---
+++ src/Dog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dog = ({image}) =>{
+    return(
+        <img src={image}/>
+    )
+}
+
+export default Dog;
