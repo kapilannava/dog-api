67c379d7e46b3a420a08c4b5afc1f6e7693e60f5
Kapilan Navaratnam <kapilan@syomortgage.com>
2020-04-19 01:32:32 -0400

    Updates

---
+++ precache-manifest.9b378722b3775595ef63c563043f7cf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa17832406846d62a5ff8ac44aac50c",
+    "url": "/kapilannava.github.io/dog-api/index.html"
+  },
+  {
+    "revision": "075888498a909c78d48c",
+    "url": "/kapilannava.github.io/dog-api/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ae2dc7b1cb4393938952",
+    "url": "/kapilannava.github.io/dog-api/static/js/2.c47767ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kapilannava.github.io/dog-api/static/js/2.c47767ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075888498a909c78d48c",
+    "url": "/kapilannava.github.io/dog-api/static/js/main.f0774e15.chunk.js"
+  },
+  {
+    "revision": "f1bdb5e71e993c5d7a96",
+    "url": "/kapilannava.github.io/dog-api/static/js/runtime-main.870ca0ca.js"
+  }
+]);
